4fd1e14d3afb7609a419bd15a25a3cd5d9420c5a
ludoed19 <temnovinc06@gmail.com>
2025-06-12 19:48:00 +0400

    Update лаба 6.py

--- лаба 6/лаба 6.py
+++ лаба 6/лаба 6.py
@@ -4,58 +4,96 @@
 # Определить (смоделировать) границы применимости рекурсивного и итерационного подхода.
 # Результаты сравнительного исследования времени вычисления представить в табличной и графической форме в виде отчета по лабораторной работе.
 # F(0) = F(1) = 1, F(n) = (-1)n*(F(n–1) /(2n)!-cos(F(n-2)+2)), при n > 1
-import math
 import timeit
 import pandas as pd
 import matplotlib.pyplot as plt
+from math import factorial, cos
 
-def sign(n):
-    return 1 if n % 2 == 0 else -1
-
-def rec_F(n, fact_cache={0: 1, 1: 1}):
+# Рекурсивная реализация
+def rec_F(n):
     if n == 0 or n == 1:
         return 1
-    # Вычисляем факториалы последовательно до 2*n
-    max_fact = max(fact_cache.keys())
-    for num in range(max_fact + 1, 2 * n + 1):
-        fact_cache[num] = fact_cache[num - 1] * num
+    sign = -1 if n % 2 else 1
+    return sign * (rec_F(n-1) / factorial(2*n) - cos(rec_F(n-2) + 2)
 
-    return sign(n) * (rec_F(n - 1, fact_cache) / fact_cache[2 * n] - math.cos(rec_F(n - 2, fact_cache) + 2))
-
+# Итерационная реализация с оптимизацией
 def iter_F(n):
     if n == 0 or n == 1:
         return 1
-    fact_cache = {0: 1, 1: 1}
-    max_fact = 1
-    values = [1, 1]
-    for i in range(2, n + 1):
-        # Последовательно вычисляем факториалы от max_fact + 1 до 2*i
-        for num in range(max_fact + 1, 2 * i + 1):
-            fact_cache[num] = fact_cache[num - 1] * num
-        max_fact = 2 * i
-
-        val = sign(i) * (values[i - 1] / fact_cache[2 * i] - math.cos(values[i - 2] + 2))
-        values.append(val)
-    return values[n]
+    
+    # Инициализация значений для n=0 и n=1
+    F = [1, 1]
+    fact = 1  # Будем накапливать факториал (2n)! итерационно
+    
+    for k in range(2, n+1):
+        # Вычисляем (2k)! = (2(k-1))! * (2k-1)*(2k)
+        fact *= (2*k-1) * (2*k)
+        sign = -1 if k % 2 else 1
+        next_val = sign * (F[-1] / fact - cos(F[-2] + 2))
+        F.append(next_val)
+    
+    return F[-1]
 
 if __name__ == '__main__':
-    ns = list(range(0, 21))
-    results = []
-
-    for n in ns:
-        t_rec = timeit.timeit(lambda: rec_F(n), number=10)
-        t_it = timeit.timeit(lambda: iter_F(n), number=10)
-        results.append((n, t_rec, t_it))
-
-    df = pd.DataFrame(results, columns=['n', 'Recursive Time (s)', 'Iterative Time (s)'])
+    # Определяем диапазон n, который мы можем вычислить за разумное время
+    max_n_rec = 15  # Максимальное n для рекурсивного метода (ограничено стеком и временем)
+    max_n_iter = 30  # Максимальное n для итерационного метода
+    
+    ns_rec = list(range(max_n_rec + 1))
+    ns_iter = list(range(max_n_iter + 1))
+    
+    # Измеряем время для рекурсивного метода
+    results_rec = []
+    for n in ns_rec:
+        try:
+            value = rec_F(n)
+            time = timeit.timeit(lambda: rec_F(n), number=10)
+            results_rec.append((n, value, time))
+        except:
+            results_rec.append((n, None, None))
+            break
+    
+    # Измеряем время для итерационного метода
+    results_iter = []
+    for n in ns_iter:
+        try:
+            value = iter_F(n)
+            time = timeit.timeit(lambda: iter_F(n), number=10)
+            results_iter.append((n, value, time))
+        except:
+            results_iter.append((n, None, None))
+            break
+    
+    # Создаем DataFrame для результатов
+    df_rec = pd.DataFrame(results_rec, columns=['n', 'Rec_F(n)', 'Rec_time (s)'])
+    df_iter = pd.DataFrame(results_iter, columns=['n', 'Iter_F(n)', 'Iter_time (s)'])
+    
+    # Объединяем результаты в одну таблицу
+    df = pd.merge(df_rec, df_iter, on='n', how='outer')
     print(df.to_string(index=False))
-
-    plt.figure(figsize=(8, 5))
-    plt.plot(df['n'], df['Recursive Time (s)'], '--o', label='Рекурсивный метод')
-    plt.plot(df['n'], df['Iterative Time (s)'], '-o', label='Итеративный метод')
+    
+    # Строим графики
+    plt.figure(figsize=(12, 6))
+    
+    # График значений
+    plt.subplot(1, 2, 1)
+    plt.plot(df['n'], df['Rec_F(n)'], 'o-', label='Рекурсия')
+    plt.plot(df['n'], df['Iter_F(n)'], 's-', label='Итерация')
     plt.xlabel('n')
-    plt.ylabel('Время выполнения (секунды)')
-    plt.title('Сравнение времени вычисления F(n)')
+    plt.ylabel('F(n)')
+    plt.title('Значения функции')
     plt.legend()
     plt.grid(True)
+    
+    # График времени
+    plt.subplot(1, 2, 2)
+    plt.plot(df['n'], df['Rec_time (s)'], 'o-', label='Рекурсия')
+    plt.plot(df['n'], df['Iter_time (s)'], 's-', label='Итерация')
+    plt.xlabel('n')
+    plt.ylabel('Время (сек)')
+    plt.title('Время вычисления')
+    plt.legend()
+    plt.grid(True)
+    
+    plt.tight_layout()
     plt.show()
